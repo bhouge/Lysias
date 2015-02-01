51f296a7bd688d07686286e4ac52d0fdd9fb420a
Ben Houge <ben@benhouge.com>
2015-02-01 18:22:21 +0100

    Not sure why IntermittentSound.js wasn't in the last commit, but here you go.

--- scripts/IntermittentSound.js
+++ scripts/IntermittentSound.js
@@ -25,20 +25,39 @@ function IntermittentSound(buffer, minPause, maxPause, minReps, maxReps, minVol,
 	var that = this;
 	
 	function playBuffer(bufferIndex, volume) {
+		//alert("insideplayBuffer");
 		var audioBufferSource = that.outputNode.context.createBufferSource();
+		//alert(audioBufferSource);
 		audioBufferSource.buffer = bufferIndex;
+		//alert(audioBufferSource.buffer);
+		//alert(that.outputNode);
+		//alert(that.outputNode.context);
 		audioBufferGain = that.outputNode.context.createGain();
+		//alert(audioBufferGain);
 		audioBufferGain.gain.value = volume;
+		//alert(audioBufferGain.gain);
 		audioBufferSource.connect(audioBufferGain);
+		//alert(audioBufferSource);
 		audioBufferGain.connect(that.outputNode);
-		audioBufferSource.start();
+		//alert("did we get this far?");
+		try {
+			audioBufferSource.start(0., 0., audioBufferSource.buffer.duration);
+		} catch(e) {
+			alert(e);
+		}
+		//alert("so now we should have started playing...");
+		
 	}
 	
 	// making this a private member function
 	function tickDownIntermittentSound() {
+		//alert("inside tickdownsound");
 		var volume = (that.maxVol - that.minVol) * Math.random() + that.minVol;
+		//alert("vol=" + volume);
 		playBuffer(that.buffer, volume);
+		//alert("just called playBuffer");
 		var bufferDur = that.buffer.duration;
+		//alert("bufferDur=" + bufferDur);
 		// ok, this will need to be replaced by having a stop message that will stop execution...
 		if (that.numberOfReps > 0 && that.isPlaying) {
 			var pauseDur = (that.maxPause - that.minPause) * 
@@ -49,9 +68,12 @@ function IntermittentSound(buffer, minPause, maxPause, minReps, maxReps, minVol,
 	}
 	
 	this.play = function() {
+		//alert("inside the play method");
 		this.isPlaying = true;
 		this.numberOfReps = Math.floor(((this.maxReps - this.minReps) + 1) * Math.random() + this.minReps);
+		//alert(this.numberOfReps);
 		tickDownIntermittentSound();
+		//alert("just called tickDownIntermittentSound");
 	}
 	
 	this.stop = function() {
@@ -60,9 +82,12 @@ function IntermittentSound(buffer, minPause, maxPause, minReps, maxReps, minVol,
 	}
 	
 	this.connect = function(nodeToConnectTo) {
+		//alert("inside the connect method");
 		try {
 			this.outputNode = nodeToConnectTo;
+			//alert("setting output node to specified node");
 		} catch(e) {
+			//alert("setting output node to destination");
 			if (nodeToConnectTo.destination) {
 				this.outputNode = nodeToConnectTo.destination;
 			} else {
